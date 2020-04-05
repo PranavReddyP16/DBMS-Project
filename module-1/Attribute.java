4716a6f8d3495fcee3455604c35df39bbe3764bc
P_Pranav_Reddy <bestpranav.16@gmail.com>
2020-04-06 01:20:04 +0530

    added attribute class

---
+++ module-1/Attribute.java
@@ -0,0 +1,19 @@
+public class Attribute
+{
+    public String name;
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public Attribute(String name)
+    {
+        setName(name);
+    }
+}

7d2d74110331996759f271fe3f5f4a0f87bddca8
AishwwaryaM <58990857+AishwwaryaM@users.noreply.github.com>
2020-04-08 08:56:09 +0530

    Update homePage.java

--- module-2/homePage.java
+++ module-2/homePage.java
@@ -83,7 +83,7 @@ public class homePage extends javax.swing.JFrame {
         search_button.setBounds(590, 30, 100, 40);
 
         jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\M SREINIVAS REDDY\\Downloads\\dbs2proj1.jpg")); // NOI18N
-        jLabel2.setText("jLabel2");
+        jLabel2.setText("");
         jPanel1.add(jLabel2);
         jLabel2.setBounds(0, 0, 840, 460);
 

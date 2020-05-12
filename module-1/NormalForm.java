a066d53c92a64c2a8ea88f49cc0737241764a1c8
P_Pranav_Reddy <bestpranav.16@gmail.com>
2020-05-13 01:06:06 +0530

    changed Normal Form so that individual normal forms are stored

--- module-1/NormalForm.java
+++ module-1/NormalForm.java
@@ -3,47 +3,61 @@ import java.util.*;
 public class NormalForm
 {
     public int NF = 1;
+    HashMap <HashMap<String, String>, Integer> individualNormalForms = new HashMap<HashMap<String, String>, Integer>();
 
     public NormalForm(HashSet<String> keys, FindKey FK)
     {
-        for(Map.Entry<String, String> entry : FK.functionalDependencyMap.entrySet())
+        for(HashMap.Entry<String, String> entry : FK.functionalDependencyMap.entrySet())
         {
+            HashMap<String, String> currentFunctionalDependency = new HashMap<String, String>();
+            currentFunctionalDependency.put(entry.getKey(), entry.getKey());
+            individualNormalForms.put(currentFunctionalDependency, 1);
             for(String j : keys)
             {
                 if(!isPrimeAttribute(entry.getValue(), keys) || Main.checkIncompleteSubstring(j,entry.getKey()))
                 {
-                    return;
+                    break;
                 }
             }
-        }
-
-        for(Map.Entry<String, String> entry : FK.functionalDependencyMap.entrySet())
-        {
-
             if(isSuperKey(entry.getKey(),keys)==false&&isPrimeAttribute(entry.getValue(),keys)==false)
             {
-                this.NF = 2;
-                return;
-            }
-
-        }
-
-        int flag=0;
-        for(String i : FK.leftHandSide)
-        {
-            if(isSuperKey(i,keys)==false)
-            {
-                flag=1;
-                NF = 3;
+                individualNormalForms.replace(currentFunctionalDependency,2);
                 break;
             }
+
+            int flag=0;
+            for(char i : entry.getKey().toCharArray())
+            {
+                if(isSuperKey(Character.toString(i),keys)==false)
+                {
+                    flag=1;
+                    individualNormalForms.replace(currentFunctionalDependency,3);
+                    break;
+                }
+            }
+            if(flag==1) return;
+            else
+            {
+                individualNormalForms.replace(currentFunctionalDependency,4);
+                return;
+            }
         }
-        if(flag==1) return;
-        else
+
+        int totalMinimumFD = 4;
+        ArrayList<HashMap<String, String>> minimumFDs = new ArrayList<HashMap<String, String>>();
+
+        for(HashMap.Entry<HashMap<String, String>, Integer> entry : individualNormalForms.entrySet())
         {
-            NF = 4;
-            return;
+            if(entry.getValue() < totalMinimumFD) totalMinimumFD = entry.getValue();
         }
+
+        for(HashMap.Entry<HashMap<String, String>, Integer> entry : individualNormalForms.entrySet())
+        {
+            if(entry.getValue()==totalMinimumFD) minimumFDs.add(entry.getKey());
+        }
+
+        this.NF = totalMinimumFD;
+
     }
 
     public boolean isSuperKey(String i,HashSet<String> keys)
